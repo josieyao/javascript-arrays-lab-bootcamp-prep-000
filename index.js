d667b84f36364dda7d6f9264bae97f084c8142e1
Josephine Yao <yao.josephine@yahoo.com>
2019-01-09 04:24:40 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -96,7 +96,7 @@ function prependKitten(name) {
   })*/
 
 function removeLastKitten() {
-  return kittens.slice(0, kittens.length - 1);
+  return kittens.slice(-1);
 }
 
 /*  describe('removeFirstKitten()', function() {
