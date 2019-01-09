66ff0bf21523824532454602a5e9a70b57dcc49e
Josephine Yao <yao.josephine@yahoo.com>
2019-01-09 04:14:04 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -90,7 +90,14 @@ function prependKitten(name) {
   return [name, ...kittens];
 }
 
+/*  describe('removeLastKitten()', function() {
+    it('removes the last kitten in the kittens array and returns a new array, leaving the kittens array unchanged', function() {
+      window.kittens = ['Milo', 'Otis', 'Garfield'];
+      expect(removeLastKitten()).toEqual(["Milo", "Otis"])
 
+      expect(window.kittens).toEqual(["Milo", "Otis", "Garfield"])
+    })
+  })*/
 
 function removeLastKitten() {
   return kittens.slice(0, kittens.length - 1);
